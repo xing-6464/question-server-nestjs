98b97b3fc7595ef969d285b317282cf0adc7a3da
xing-6464 <17621404964@163.com>
2024-06-05 10:01:57 +0800

    路由常见功能：query，param和request-body part1

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
   @Get()
-  findAll() {
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    console.log(keyword, page, pageSize);
     return {
       list: ['a', 'b', 'c'],
       count: 10,
+    };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id);
+    return 'question test';
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() question: QuestionDto) {
+    console.log(question);
+    return {
+      id,
+      title: 'aaa',
+      desc: 'ccc',
     };
   }
 }

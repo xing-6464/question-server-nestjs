c29638aa4fa95ba6c391ceec777ac9e123c2572f
xing-6464 <17621404964@163.com>
2024-06-11 19:15:34 +0800

    Redirect重定向user路由，以符合现有的API设计规范

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,21 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Redirect,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() userDto: CreateUserDto) {
     try {
@@ -19,5 +23,18 @@ export class UserController {
     } catch (error) {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Get('info')
+  @Redirect('/api/auth/profile', 302) // http状态码 GET请求 - 301永久 302临时
+  async info() {
+    return;
+  }
+
+  @Public()
+  @Post('login')
+  @Redirect('/api/auth/login', 307) // http状态码 POST请求 - 308永久 307临时
+  async login() {
+    return;
   }
 }
